38a60d3529b748b61b98f6be0038406368546194
Udai Das <udaid347@gmail.com>
2025-08-11 11:38:45 +0530

    Fix import statements and formatting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
-import Footer from './sections/Footer';
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -18,7 +17,7 @@ const App = () => {
       <Experiences />
       <Testimonial />
       <Contact />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
